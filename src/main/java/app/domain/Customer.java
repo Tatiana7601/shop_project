8f9e8d76fa4f67daedf129e569adadd2fcef30fb
19nfyZ76 <paltatyana1@gmail.com>
2025-06-24 12:58:32 +0200

    init

---
+++ src/main/java/app/domain/Customer.java
@@ -0,0 +1,66 @@
+package app.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Customer {
+    private final Long id;
+    private boolean isActive;
+    private  String name;
+    private  final List<Product> products = new ArrayList<>();
+
+    public Customer(Long id, String name, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.isActive = isActive;
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return isActive == customer.isActive && Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(products, customer.products);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isActive, name, products);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Customer{");
+        sb.append("id=").append(id);
+        sb.append(", isActive=").append(isActive);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", products=").append(products);
+        sb.append('}');
+        return sb.toString();
+    }
+}
